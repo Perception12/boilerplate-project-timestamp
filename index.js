c8cb5d6353d251ac966b81b70c36d85b0b3e7796
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-07-28 09:57:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:dateString", (req, res) => {
+app.get("/api/:dateString?", (req, res) => {
   const dateString = req.params.dateString;
   let date;
   if (!dateString) {
