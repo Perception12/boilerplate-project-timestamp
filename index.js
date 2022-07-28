7799cbe30dab5925439db9330385741d6fa2455a
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-07-28 09:56:52 +0100

    modified index.js

--- index.js
+++ index.js
@@ -24,7 +24,25 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:dateString", (req, res) => {
+  const dateString = req.params.dateString;
+  let date;
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateString)){
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+  
+  if (date.toString == "Invalid Date") {
+    res.json({error: "Invalid Date"});
+  } else {
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
